42268ca209c8cfbea15877680a3969039c4f8b1c
Kacper Stroisz <kacperstroisz@wp.pl>
2018-06-01 13:30:18 +0200

    Add files

---
+++ src/components/Player/Player.test.js
@@ -0,0 +1,17 @@
+import Player from './Player';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+it('renders without crashing', () => {
+    shallow( < Player / > );
+});
+
+it('renders correct name', () => {
+            const playerNamePassed = 'Ania';
+            const playerComponent = shallow( < Player name = { playerNamePassed }
+                />);
+
+                const playerNameRendered = playerComponent.find('.Player__name').text();
+
+                expect(playerNameRendered).toEqual(playerNamePassed);
+            });
